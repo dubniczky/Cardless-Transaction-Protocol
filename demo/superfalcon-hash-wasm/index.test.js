3b855d3c7b517177c881765d2ec428c0edbb0040
Krisztofer Zoltán Horvát <krisztofer0605@gmail.com>
2023-05-06 14:47:25 +0200

    Move superfalcon test to mocha

--- demo/superfalcon-hash-wasm/index.test.js
+++ demo/superfalcon-hash-wasm/index.test.js
@@ -1,43 +1,51 @@
+const superFalcon = require('.')
+const assert = require('assert')
+
 const hashTypes = [
     'sha512', 'sha-512', 'sha2-512', 'sha3512', 'sha3-512'
 ]
-
-const superFalcon = require('.')
 
 const message = new Uint8Array([98, 97, 108, 108, 115, 0]);
 
 const toHex = bytes => Buffer.from(bytes).toString('hex');
 
-test('key pair generation', async () => superFalcon.keyPair());
+describe('Key generation', function () {
+    it('key pair generation', async function () {
+        superFalcon.keyPair()
+    })
+})
 
-for (const hashType of hashTypes) {
-	test(`end-to-end test of ${hashType}`, async () => {
-		const keyPair = await superFalcon.keyPair();
 
-		const signed = await superFalcon.sign(
-            hashType,
-			message,
-			keyPair.privateKey
-		);
-		const verified = await superFalcon.open(
-            hashType,
-			signed,
-			keyPair.publicKey
-		);
+describe('End-to-end test', function () {
+    for (const hashType of hashTypes) {
+        it(`hashType: ${hashType}`, async function () {
+            const keyPair = await superFalcon.keyPair();
 
-		const signature = await superFalcon.signDetached(
-            hashType,
-			message,
-			keyPair.privateKey
-		);
-		const isValid = await superFalcon.verifyDetached(
-            hashType,
-			signature,
-			message,
-			keyPair.publicKey
-		);
+            const signed = await superFalcon.sign(
+                hashType,
+                message,
+                keyPair.privateKey
+            )
+            const verified = await superFalcon.open(
+                hashType,
+                signed,
+                keyPair.publicKey
+            )
 
-		expect(toHex(verified)).toBe(toHex(message));
-		expect(isValid).toBe(true);
-	});
-}
+            const signature = await superFalcon.signDetached(
+                hashType,
+                message,
+                keyPair.privateKey
+            )
+            const isValid = await superFalcon.verifyDetached(
+                hashType,
+                signature,
+                message,
+                keyPair.publicKey
+            )
+
+            assert.equal(toHex(message), toHex(verified))
+            assert.ok(isValid)
+        })
+    }
+})
